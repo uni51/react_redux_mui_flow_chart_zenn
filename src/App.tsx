faa200a16905d66e98edd8ce24f7ffb2d6411253
uni51 <unieye51@gmail.com>
2024-04-25 22:11:48 +0900

    第4章 MUIをさわっておく

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Button from "./components/util/Button";
+import Checkbox from "./components/util/Checkbox";
+import TextField from "./components/util/TextField";
 
 interface AppProps {}
 const App: FC<AppProps> = () => {
-  return <div>flowchart builder !</div>;
+  return (
+    <div>
+      flowchart builder !<Button>aaa</Button>
+      <Checkbox />
+      <TextField />
+    </div>
+  );
 };
 
 export default App;
